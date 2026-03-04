1b03c5d829d0f837538da65cea1c58f5ada7bee9
Sebetola Junior <juniorsebetola253@gmail.com>
2026-03-05 01:41:34 +0200

    this code version enables threaded mode in Flask

--- airsend.py
+++ airsend.py
@@ -67,4 +67,4 @@ if __name__ == "__main__":
     qr.make(fit=True)
     qr.make_image(fill_color="black", back_color="white").show()
 
-    app.run(host="0.0.0.0", port=PORT)
+    app.run(host="0.0.0.0", port=PORT,threaded=True)
